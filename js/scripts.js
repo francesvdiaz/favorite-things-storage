5396cf00965c94b3ee88ec75f6a9b01a4e013783
FrancesDiaz <115190000+francesvdiaz@users.noreply.github.com>
2023-02-01 20:31:27 -0800

    js elements working correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,29 @@
-window.addEventListener("load", function(event) {
-  event.preventDefault;
-  
-})
+window.addEventListener("load", function() {
+  const button = document.getElementById("enter")
+
+  button.addEventListener("click", function(event){
+    event.preventDefault();
+
+    let fav1 = document.getElementById("input1").value;
+    let fav2 = document.getElementById("input2").value;
+    let fav3 = document.getElementById("input3").value;
+
+    const favArray = [fav1,fav2,fav3];
+    
+    const list = document.createElement("ul");
+    body = document.querySelector("body");
+    body.append(list);
+
+    let li1 = document.createElement("li");
+    let li2 = document.createElement("li");
+    let li3 = document.createElement("li");
+
+    li1.append(favArray[0])
+    li2.append(favArray[2]);
+    li3.append(favArray[1]);
+
+    list.append(li1,li2,li3);
+    
+
+  });
+});
